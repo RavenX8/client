f206aa3120edbee824fd504795083fb48da6ef59
Christopher Torres <RavenX8@users.noreply.github.com>
2019-07-03 19:23:51 -0400

    znzin now compiles trose.exe needs some love

---
+++ cmake/modules/Build-ijl15.cmake
@@ -0,0 +1,21 @@
+set(IJL15_INSTALL_DIR ${CMAKE_THIRD_PARTY_DIR}/ijl-1.5)
+
+set(_byproducts
+  ${IJL15_INSTALL_DIR}/IJL15.lib
+)
+
+if(WIN32)
+  set(IJL15_LIBRARY "${IJL15_INSTALL_DIR}/IJL15.lib")
+  set(IJL15_LIBRARIES "${LUA_LIBRARY}")
+endif()
+
+set(IJL15_INCLUDE_DIR "${IJL15_INSTALL_DIR}/ijl")
+
+if(NOT TARGET utils::ijl15)
+  add_library(utils::ijl15 INTERFACE IMPORTED)
+  add_dependencies(utils::ijl15 ijl15)
+  set_target_properties(utils::ijl15 PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${IJL15_INCLUDE_DIR}")
+  set_target_properties(utils::ijl15 PROPERTIES INTERFACE_LINK_LIBRARIES "${IJL15_LIBRARIES}")
+endif()
+
+mark_as_advanced(IJL15_INCLUDE_DIR IJL15_LIBRARY IJL15_LIBRARIES)
--- cmake/modules/Build-lua.cmake
+++ cmake/modules/Build-lua.cmake
@@ -1,67 +1,156 @@
 set(LUA_INSTALL_DIR ${CMAKE_THIRD_PARTY_DIR})
 
 set(_byproducts
-  ${LUA_INSTALL_DIR}/lib/lua.lib
-  ${LUA_INSTALL_DIR}/lib/liblua.a
+  ${LUA_INSTALL_DIR}/lib/liblua4.lib
+  ${LUA_INSTALL_DIR}/lib/liblua4.a
+  ${LUA_INSTALL_DIR}/lib/liblua5.lib
+  ${LUA_INSTALL_DIR}/lib/liblua5.a
 )
 
-if(WIN32)  
+if(WIN32)
   ExternalProject_Add(
-    lua
-    GIT_REPOSITORY https://github.com/RavenX8/lua.git
-    GIT_TAG e3f46c3bee3d6b62f9ea9a36c86249978504e404
-    CMAKE_ARGS -G${CMAKE_GENERATOR} -Dlua_MSVC_STATIC_RUNTIME=ON -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
+    lua4
+    DOWNLOAD_COMMAND ""
+    SOURCE_DIR ${CMAKE_THIRD_PARTY_DIR}/lua-4.0.1
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND cl /c /I <SOURCE_DIR>/include /EHsc <SOURCE_DIR>/src/lua/lua.c 
+    <SOURCE_DIR>/src/lapi.c
+    <SOURCE_DIR>/src/lcode.c
+    <SOURCE_DIR>/src/ldebug.c
+    <SOURCE_DIR>/src/ldo.c
+    <SOURCE_DIR>/src/lfunc.c
+    <SOURCE_DIR>/src/lgc.c
+    <SOURCE_DIR>/src/lib/lauxlib.c
+    <SOURCE_DIR>/src/lib/lbaselib.c
+    <SOURCE_DIR>/src/lib/ldblib.c
+    <SOURCE_DIR>/src/lib/liolib.c
+    <SOURCE_DIR>/src/lib/lmathlib.c
+    <SOURCE_DIR>/src/lib/lstrlib.c
+    <SOURCE_DIR>/src/llex.c
+    <SOURCE_DIR>/src/lmem.c
+    <SOURCE_DIR>/src/lobject.c
+    <SOURCE_DIR>/src/lparser.c
+    <SOURCE_DIR>/src/lstate.c
+    <SOURCE_DIR>/src/lstring.c
+    <SOURCE_DIR>/src/ltable.c
+    <SOURCE_DIR>/src/ltests.c
+    <SOURCE_DIR>/src/ltm.c
+    <SOURCE_DIR>/src/lundump.c
+    <SOURCE_DIR>/src/lvm.c
+    <SOURCE_DIR>/src/lzio.c
+    && lib *.obj /OUT:${LUA_INSTALL_DIR}/lib/liblua4.lib
+    INSTALL_COMMAND ""
+    BUILD_IN_SOURCE true
     INSTALL_DIR ${LUA_INSTALL_DIR}
     BUILD_BYPRODUCTS ${_byproducts}
   )
   
-  ExternalProject_Add_Step(
-    lua
-    copy-lua
-    DEPENDEES download
-    COMMAND ${CMAKE_SCRIPT_PATH}/robocopy.bat "<SOURCE_DIR>/src" "<INSTALL_DIR>/include/" "*.h"
-    COMMAND ${CMAKE_SCRIPT_PATH}/robocopy.bat "<SOURCE_DIR>/src" "<INSTALL_DIR>/include/" "*.hpp"
-  )
-else()
-  find_library(LUA_DL_LIBRARY dl)
-  find_library(LUA_MATH_LIBRARY m)
   ExternalProject_Add(
-    lua
-    URL https://www.lua.org/ftp/lua-5.3.4.tar.gz
-    URL_HASH SHA1=79790cfd40e09ba796b01a571d4d63b52b1cd950
-    INSTALL_DIR ${LUA_INSTALL_DIR}
-    BUILD_IN_SOURCE TRUE
-
+    lua5
+    DOWNLOAD_COMMAND ""
+    SOURCE_DIR ${CMAKE_THIRD_PARTY_DIR}/lua-5.0.0
     CONFIGURE_COMMAND ""
-    BUILD_COMMAND make linux
-    INSTALL_COMMAND make install INSTALL_TOP=<INSTALL_DIR>
+    BUILD_COMMAND cl /c /I <SOURCE_DIR>/include /EHsc <SOURCE_DIR>/src/lua/lua.c
+    <SOURCE_DIR>/src/lapi.c
+    <SOURCE_DIR>/src/lcode.c
+    <SOURCE_DIR>/src/ldebug.c
+    <SOURCE_DIR>/src/ldo.c
+    <SOURCE_DIR>/src/ldump.c
+    <SOURCE_DIR>/src/lfunc.c
+    <SOURCE_DIR>/src/lgc.c
+    <SOURCE_DIR>/src/lib/lauxlib.c
+    <SOURCE_DIR>/src/lib/lbaselib.c
+    <SOURCE_DIR>/src/lib/ldblib.c
+    <SOURCE_DIR>/src/lib/liolib.c
+    <SOURCE_DIR>/src/lib/lmathlib.c
+    <SOURCE_DIR>/src/lib/loadlib.c
+    <SOURCE_DIR>/src/lib/lstrlib.c
+    <SOURCE_DIR>/src/lib/ltablib.c
+    <SOURCE_DIR>/src/llex.c
+    <SOURCE_DIR>/src/lmem.c
+    <SOURCE_DIR>/src/lobject.c
+    <SOURCE_DIR>/src/lopcodes.c
+    <SOURCE_DIR>/src/lparser.c
+    <SOURCE_DIR>/src/lstate.c
+    <SOURCE_DIR>/src/lstring.c
+    <SOURCE_DIR>/src/ltable.c
+    <SOURCE_DIR>/src/ltests.c
+    <SOURCE_DIR>/src/ltm.c
+    <SOURCE_DIR>/src/lundump.c
+    <SOURCE_DIR>/src/lvm.c
+    <SOURCE_DIR>/src/lzio.c
+    && lib *.obj /OUT:${LUA_INSTALL_DIR}/lib/liblua5.lib
+    INSTALL_COMMAND ""
+    BUILD_IN_SOURCE true
+    INSTALL_DIR ${LUA_INSTALL_DIR}
     BUILD_BYPRODUCTS ${_byproducts}
   )
+else()
+#  find_library(LUA_DL_LIBRARY dl)
+#  find_library(LUA_MATH_LIBRARY m)
+#  ExternalProject_Add(
+#    lua5
+#    URL https://www.lua.org/ftp/lua-5.3.4.tar.gz
+#    URL_HASH SHA1=79790cfd40e09ba796b01a571d4d63b52b1cd950
+#    INSTALL_DIR ${LUA_INSTALL_DIR}
+#    BUILD_IN_SOURCE TRUE
+#
+#    CONFIGURE_COMMAND ""
+#    BUILD_COMMAND make linux
+#    INSTALL_COMMAND make install INSTALL_TOP=<INSTALL_DIR>
+#    BUILD_BYPRODUCTS ${_byproducts}
+#  )
 endif()
 
 ExternalProject_Get_Property(
-  lua
+  lua4
+  source_dir
+  install_dir
+)
+if(WIN32)
+  set(LUA_LIBRARY "${install_dir}/lib/liblua4.lib")
+  set(LUA_LIBRARIES "${LUA_LIBRARY}")
+  set(LUA_INSTALL_LIBS "${install_dir}/lib/liblua.dll")
+else()
+#  set(LUA_LIBRARY "${install_dir}/lib/liblua4.a")
+#  set(LUA_LIBRARIES "${LUA_LIBRARY}" "${LUA_DL_LIBRARY}" "${LUA_MATH_LIBRARY}")
+#  set(LUA_INSTALL_LIBS "${install_dir}/lib/liblua4.a")
+endif()
+
+set(LUA_INCLUDE_DIR "${source_dir}/include")
+
+if(NOT TARGET lua::lua4)
+  add_library(lua::lua4 INTERFACE IMPORTED)
+  add_dependencies(lua::lua4 lua4)
+  set_target_properties(lua::lua4 PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${LUA_INCLUDE_DIR}")
+  set_target_properties(lua::lua4 PROPERTIES INTERFACE_LINK_LIBRARIES "${LUA_LIBRARIES}")
+endif()
+
+#Lua 5 stoof
+ExternalProject_Get_Property(
+  lua5
+  source_dir
   install_dir
 )
 
 if(WIN32)
-  set(LUA_LIBRARY "${install_dir}/lib/lua.lib")
+  set(LUA_LIBRARY "${install_dir}/lib/liblua5.lib")
   set(LUA_LIBRARIES "${LUA_LIBRARY}")
   set(LUA_INSTALL_LIBS "${install_dir}/lib/liblua.dll")
 else()
-  set(LUA_LIBRARY "${install_dir}/lib/liblua.a")
-  set(LUA_LIBRARIES "${LUA_LIBRARY}" "${LUA_DL_LIBRARY}" "${LUA_MATH_LIBRARY}")
-  set(LUA_INSTALL_LIBS "${install_dir}/lib/liblua.a")
+#  set(LUA_LIBRARY "${install_dir}/lib/liblua5.a")
+#  set(LUA_LIBRARIES "${LUA_LIBRARY}" "${LUA_DL_LIBRARY}" "${LUA_MATH_LIBRARY}")
+#  set(LUA_INSTALL_LIBS "${install_dir}/lib/liblua5.a")
 endif()
 
 
-set(LUA_INCLUDE_DIR "${install_dir}/include")
+set(LUA_INCLUDE_DIR "${source_dir}/include")
 
-if(NOT TARGET lua::lua)
-    add_library(lua::lua INTERFACE IMPORTED)
-    add_dependencies(lua::lua lua)
-    set_target_properties(lua::lua PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${LUA_INCLUDE_DIR}")
-    set_target_properties(lua::lua PROPERTIES INTERFACE_LINK_LIBRARIES "${LUA_LIBRARIES}")
+if(NOT TARGET lua::lua5)
+    add_library(lua::lua5 INTERFACE IMPORTED)
+    add_dependencies(lua::lua5 lua5)
+    set_target_properties(lua::lua5 PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${LUA_INCLUDE_DIR}")
+    set_target_properties(lua::lua5 PROPERTIES INTERFACE_LINK_LIBRARIES "${LUA_LIBRARIES}")
 endif()
 
-mark_as_advanced(LUA_INCLUDE_DIR LUA_LIBRARY LUA_LIBRARIES)
+#mark_as_advanced(LUA_INCLUDE_DIR LUA_LIBRARY LUA_LIBRARIES)
