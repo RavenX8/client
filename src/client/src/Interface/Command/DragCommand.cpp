06df1a397bd79eb126c991cd175e86365a503c67
Davidixx <b1tchaccplz@gmail.com>
2021-02-06 21:09:48 +0200

    ....

--- src/client/src/Interface/Command/DragCommand.cpp
+++ src/client/src/Interface/Command/DragCommand.cpp
@@ -310,6 +310,9 @@ bool CTCmdDragItemFromInvenInItemDlg::Exec(CTObject* pObj) {
       pSourceSlot->DetachIcon();
       pTargetSlot->AttachIcon( pSourceItem->CreateItemIcon() );
     }
+    //TODO: Switch item places is here :>
+    //pTempItem.GetIndex(); //davidixx
+    //pSourceItem.GetIndex(); //davidixx
   }
 
   return true;
